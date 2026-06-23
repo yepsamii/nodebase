20657a96b3bbd8f90efd4a12c9142c365ba70c97
yepsamii <abrahmansw@gmail.com>
2026-06-23 10:07:04 +0600

    Refactor authentication layout to use a dedicated AuthLayout component

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,26 +1,7 @@
-import Image from "next/image";
-import Link from "next/link";
+import AuthLayout from "@/features/auth/components/AuthLayout";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="flex p-16 w-full justify-center items-center min-h-screen">
-      <div className="w-full flex flex-col gap-4 item-center max-w-sm">
-        <Link
-          href={"/"}
-          className="flex gap-2 items-center font-medium"
-        >
-          <Image
-            src={"/logos/logo.svg"}
-            alt="Nodebase"
-            height={24}
-            width={24}
-          />
-          Nodebase
-        </Link>
-        {children}
-      </div>
-    </div>
-  );
+  return <AuthLayout>{children}</AuthLayout>;
 };
 
 export default Layout;
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -3,7 +3,6 @@ import { requireUnAuth } from "@/lib/auth-utils";
 
 const Page = async () => {
   await requireUnAuth();
-
   return <RegisterForm />;
 };
 
